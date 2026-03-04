fcdc888e59eb85dd3c67ac9e7d6a69417e978669
Nguyen Vu <vunahe160147@fpt.edu.vn>
2026-03-04 15:49:32 +0700

    Remove [NonController] attribute from VehicleAssetsController

--- PTJ_API/Controllers/VehicleAssets/VehicleAssetsController.cs
+++ PTJ_API/Controllers/VehicleAssets/VehicleAssetsController.cs
@@ -11,7 +11,7 @@ namespace API.Controllers.VehicleAssets;
 
 [ApiController]
 [Authorize]
-[NonController]
+
 [Route("api/assets/vehicles")]
 public sealed class VehicleAssetsController : ControllerBase
 {
