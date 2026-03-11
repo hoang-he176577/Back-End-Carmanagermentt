91bae211343c0b28231357c458ed3a4163b10c12
TuanHung <tranthohung100302@gmail.com>
2026-03-11 15:58:21 +0700

    Revert "ddddd"
    This reverts commit 37b6f1f127be494c6146f0db88df8ed0f2f06a2c.

--- PTJ_API/Program.cs
+++ PTJ_API/Program.cs
@@ -91,7 +91,12 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IEmailSender, SmtpEmailSender>();
 builder.Services.AddScoped<IPurchaseProposalService, PurchaseProposalService>();
+<<<<<<< Updated upstream
+
+=======
 builder.Services.AddScoped<ITripLogsService, TripLogService>();
+builder.Services.AddScoped<IBranchService, BranchService>();
+>>>>>>> Stashed changes
 
 // 🔥 ADD USER REPO & SERVICE
 builder.Services.AddScoped<IUserRepository, UserRepository>();
