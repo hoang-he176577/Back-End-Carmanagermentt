9206f2cc3b0c7f59ef80985f9f61045eeaf94b6d
TuanHung <tranthohung100302@gmail.com>
2026-03-13 13:30:51 +0700

    .

--- PTJ_Models/Models/CarManagerContext.cs
+++ PTJ_Models/Models/CarManagerContext.cs
@@ -62,8 +62,9 @@ public partial class CarManagerContext : DbContext
     public virtual DbSet<VehicleModel> VehicleModels { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server =localhost; database = CarManager; uid=sa; pwd=123456;Trusted_Connection=True;Encrypt=False");
+    {
+
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
